86f6df239a4796ece15a737a77e667a48c90a432
Theo Springer <theo.springer@edu.univ-fcomte.fr>
2024-10-23 14:22:18 +0200

    full changement

--- script_panier.js
+++ script_panier.js
@@ -12,7 +12,7 @@ function updateTotal() {
     const game1Quantity = parseInt(document.getElementById('quantity-game1').textContent);
     const game2Quantity = parseInt(document.getElementById('quantity-game2').textContent);
     const subtotal = (59.99 * game1Quantity + 54.99 * game2Quantity).toFixed(2);
-    const taxes = (subtotal * 0.2).toFixed(2);
+    const taxes = (subtotal * 0.02).toFixed(2);
     const total = (parseFloat(subtotal) + parseFloat(taxes)).toFixed(2);
 
     document.getElementById('subtotal').textContent = `${subtotal} €`;
